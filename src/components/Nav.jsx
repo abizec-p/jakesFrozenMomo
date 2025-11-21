aa3bb2edf879fdf1c24cd3686ca9ee975d2001d6
Abishek pariyar- portfolio <swaggyabii56@gmail.com>
2025-11-21 04:06:04 -0500

    first mobile setup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,14 @@ import { Link } from "react-router-dom";
 import logo from "../assets/img/light-jake.svg";
 
 import "./nav.css";
+import { useState } from "react";
 export default function Nav() {
+  const [isClicked, setIsClicked] = useState("false")
+
+  function HandleButton(){
+    setIsClicked(!isClicked);
+  }
+
   return (
     <section>
       <div className="navigation-bar">
@@ -13,11 +20,16 @@ export default function Nav() {
             </Link>
           </div>
         </div>
-        <div className="nav-right">
+        <div className={isClicked? "nav-right":"nav-clicked"}>
           <Link to="/">Home</Link>
           <Link to="/prices">Prices</Link>
           <Link to="/about">About us</Link>
           <Link to="/contact">Contact Us</Link>
+        </div>
+        <div onClick={HandleButton} className="hamburger">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     </section>
