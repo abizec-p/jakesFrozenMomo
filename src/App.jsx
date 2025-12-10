fd5f12429f0b2e533f082a824a376634bd572013
Abishek pariyar- portfolio <swaggyabii56@gmail.com>
2025-12-10 01:38:54 -0500

    nre

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import Nav from "./Nav"
-import Home from "./components/Home"
-import {Routes, Route} from "react-router-dom"
-import Footer from "./components/footer"
-import Prices from "./components/prices"
-import About from "./components/About"
-import Contact from "./components/contact"
-import PrivacyPolicy from "./components/privacy"
+import Nav from "./Nav";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/footer.jsx";
+import Prices from "./components/prices.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/contact.jsx";
+import PrivacyPolicy from "./components/Privacy.jsx";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <section>
-      <Nav/>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/prices" element={<Prices/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/privacy" element={<PrivacyPolicy/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/prices" element={<Prices />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
       </Routes>
-      <Footer/>
-      
-
-
+      <Footer />
     </section>
-  )
+  );
 }
